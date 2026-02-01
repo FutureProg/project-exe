7a299bf126d43bbbe2d5ae8de4378eaf4e485be7
Nick Morrison <nickmorrison09@gmail.com>
2026-02-01 01:23:24 -0500

    Added simple prompt to test SDK, and updated default permissions

--- server/main.ts
+++ server/main.ts
@@ -1,5 +1,32 @@
+import { query } from "@anthropic-ai/claude-agent-sdk";
+
 if (import.meta.main) {
-  Deno.serve((_req) => {
-    return new Response("Hello World", { status: 200 });
+  const currentDateTime = new Date(Date.now());
+  Deno.serve(async (_req) => {
+    const queryJob = query({
+      prompt: "Hello, Claude! How are you today? And what time is it?",
+      options: {
+        model: "claude-sonnet-4-5",
+        sandbox: {
+          enabled: true,
+          autoAllowBashIfSandboxed: true,
+        },
+        allowedTools: ["Bash", "Bash(time)"],
+        systemPrompt: {
+          type: "preset",
+          preset: "claude_code",
+          append: `\n The current time is ${currentDateTime.toLocaleDateString()} ${currentDateTime.toLocaleTimeString()}.`,
+        }
+      },
+    });
+    for await (const message of queryJob) {
+      console.log(message);
+      if ("result" in message) {
+        return new Response(`Claude responded: ${message.result}`, {
+          status: 200,
+        });
+      }
+    }
+    return new Response("No response.", { status: 200 });
   });
 }
